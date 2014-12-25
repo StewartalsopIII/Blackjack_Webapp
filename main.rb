9335abc41a659f5b2c372e73be6a2465766f8c8b
Stewart Alsop <stewartalsopIII@gmail.com>
2014-12-25 10:07:33 +0800

    Finish initial game setup

--- main.rb
+++ main.rb
@@ -4,14 +4,36 @@ require 'pry'
 
 set :sessions, true
 
-get '/test' do 
-  erb :test
+get '/' do 
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/player_new'
+  end
 end
 
-get '/nested_template' do 
-  erb :"/users/profile"
+get '/player_new' do
+  erb :player_new
 end
 
+post '/player_new' do 
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
 
+get '/game' do
 
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
+  erb :game
+end
 
